8537391033480b3c576b7a96d5667ec03dcbe2ba
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-12-02 09:06:04 +0100

    Optimized day1

--- crates/day1/src/day1.rs
+++ crates/day1/src/day1.rs
@@ -1,16 +1,30 @@
 use files::load_uint_vec_from;
+use std::collections::HashMap;
 
-pub fn problem1() -> u32 {
+pub fn problem1() -> i64 {
     let numbers = load_uint_vec_from("crates/day1/src/day1_input.txt");
+    let mut pairs = HashMap::new();
+    let year: i64 = 2020;
+
+    match numbers {
+        Ok(ref nums) =>
+            for n in nums.iter().copied() {
+                pairs.insert(year - n, n);
+            },
+        _ => panic!()
+    }
 
     let mut result = 0;
     match numbers {
-        Ok(v) => for num in v.iter() {
-            for num2 in v.iter() {
-                if num + num2 == 2020 {
-                    result = num * num2;
-                    break;
-                }
+        Ok(ref v) => for num in v.iter() {
+            let x = match pairs.get(num) {
+                Some(n) => *n,
+                _ => 0
+            };
+
+            if num + x == year {
+                result = num * x;
+                break;
             }
         },
         Err(e) => println!("ERROR, {}", e)
@@ -18,19 +32,39 @@ pub fn problem1() -> u32 {
     result
 }
 
-pub fn problem2() -> u32 {
+pub fn problem2() -> i64 {
     let numbers = load_uint_vec_from("crates/day1/src/day1_input.txt");
+    let mut pairs = HashMap::new();
+    let mut products = HashMap::new();
+    let year: i64 = 2020;
+
+    match numbers {
+        Ok(ref nums) =>
+            for n in nums.iter().copied() {
+                for n2 in nums.iter().copied() {
+                    pairs.insert(year - n - n2, n + n2);
+                    products.insert(year - n - n2, n * n2);
+                }
+            },
+        _ => panic!()
+    }
 
     let mut result = 0;
     match numbers {
-        Ok(v) => for num in v.iter() {
-            for num2 in v.iter() {
-                for num3 in v.iter() {
-                    if num + num2 + num3 == 2020 {
-                        result = num * num2 * num3;
-                        break;
-                    }
-                }
+        Ok(ref v) => for num in v.iter() {
+            let x = match pairs.get(num) {
+                Some(n) => *n,
+                _ => 0
+            };
+
+            let product = match products.get(num) {
+                Some(n) => *n,
+                _ => 0
+            };
+
+            if num + x == year {
+                result = num * product;
+                break;
             }
         },
         Err(e) => println!("ERROR, {}", e)
--- crates/files/src/lib.rs
+++ crates/files/src/lib.rs
@@ -1,12 +1,12 @@
 use std::fs;
 use std::io::{BufReader, BufRead, Error, ErrorKind};
 
-pub fn load_uint_vec_from(filename: &str) -> Result<Vec<u32>, Error> {
+pub fn load_uint_vec_from(filename: &str) -> Result<Vec<i64>, Error> {
     let file = fs::File::open(filename).expect("File not found");
     let br = BufReader::new(file);
 
     return br.lines()
         .map(|line| line.expect("No line"))
-        .map(|num| num.parse::<u32>().map_err(|e| Error::new(ErrorKind::InvalidData, e)))
+        .map(|num| num.parse::<i64>().map_err(|e| Error::new(ErrorKind::InvalidData, e)))
         .collect();
 }
