a50bcfdcc6cda6d9d112d334257b41feafa4e2f7
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-12-03 16:29:23 +0100

    Finished problem 2 of day 3

--- crates/day3/src/day3.rs
+++ crates/day3/src/day3.rs
@@ -1,24 +1,26 @@
 use files::load_str_vec_from;
 
-pub fn problem1() -> i32 {
+pub fn problem1(right: usize, down: usize) -> i64 {
     let forest_result = load_str_vec_from("crates/day3/src/day3_input.txt");
     let mut trees = 0;
 
     if let Ok(forest) = forest_result {
-        let mut right = 0;
-        for line in forest.iter() {
-            let chars = line.chars().collect::<Vec<char>>();
+        let mut right_cont = 0;
+        for (i, line) in forest.iter().enumerate() {
+            if i % down == 0 {
+                let chars = line.chars().collect::<Vec<char>>();
 
-            let cell = chars[right];
+                let cell = chars[right_cont];
 
-            if cell == '#' {
-                trees = trees + 1;
-            }
+                if cell == '#' {
+                    trees = trees + 1;
+                }
 
-            right += 3;
+                right_cont += right;
 
-            if right > chars.len() - 1 {
-                right %= chars.len();
+                if right_cont > chars.len() - 1 {
+                    right_cont %= chars.len();
+                }
             }
         }
     }
@@ -27,5 +29,9 @@ pub fn problem1() -> i32 {
 }
 
 pub fn problem2() -> i64 {
-    0
+    problem1(1, 1) *
+        problem1(3, 1) *
+        problem1(5, 1) *
+        problem1(7, 1) *
+        problem1(1, 2)
 }
--- crates/day3/src/main.rs
+++ crates/day3/src/main.rs
@@ -1,6 +1,6 @@
 mod day3;
 
 fn main() {
-    println!("Day 3, Problem 1: {}", day3::problem1());
+    println!("Day 3, Problem 1: {}", day3::problem1(3, 1));
     println!("Day 3, Problem 2: {}", day3::problem2());
 }
