23bc5d013bb2083584fbcbe4a7f6b850a0351720
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-12-03 15:57:51 +0100

    Day 3 problem 1 resolved

--- crates/day3/src/day3.rs
+++ crates/day3/src/day3.rs
@@ -1,5 +1,29 @@
+use files::load_str_vec_from;
+
 pub fn problem1() -> i32 {
-    0
+    let forest_result = load_str_vec_from("crates/day3/src/day3_input.txt");
+    let mut trees = 0;
+
+    if let Ok(forest) = forest_result {
+        let mut right = 0;
+        for line in forest.iter() {
+            let chars = line.chars().collect::<Vec<char>>();
+
+            let cell = chars[right];
+
+            if cell == '#' {
+                trees = trees + 1;
+            }
+
+            right += 3;
+
+            if right > chars.len() - 1 {
+                right %= chars.len();
+            }
+        }
+    }
+
+    trees
 }
 
 pub fn problem2() -> i64 {
