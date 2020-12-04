fd8592151207315cea4dd1e79023244f872f0f83
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-12-04 08:41:04 +0100

    Initial commit for day 4

---
+++ crates/day4/src/day4.rs
@@ -0,0 +1,12 @@
+use files::load_str_vec_from;
+
+pub fn problem1() -> i64 {
+    let passports_loaded = load_str_vec_from("crates/day4/src/day4_input.txt");
+    let mut valid_passports = 0;
+
+    valid_passports
+}
+
+pub fn problem2() -> i64 {
+    0
+}
---
+++ crates/day4/src/main.rs
@@ -0,0 +1,6 @@
+mod day4;
+
+fn main() {
+    println!("Day 4, Problem 1: {}", day4::problem1());
+    println!("Day 4, Problem 2: {}", day4::problem2());
+}
