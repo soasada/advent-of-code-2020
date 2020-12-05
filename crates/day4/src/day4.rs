a602e4757d8b6d80275c4baf2c53b93d6357d263
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-12-05 15:43:39 +0100

    Added validations for every passport field

--- crates/day4/src/day4.rs
+++ crates/day4/src/day4.rs
@@ -1,4 +1,6 @@
 use files::load_str_vec_from;
+use std::collections::HashMap;
+use regex::Regex;
 
 pub fn problem1() -> i64 {
     let passports_loaded = load_str_vec_from("crates/day4/src/day4_input.txt");
@@ -19,7 +21,7 @@ pub fn problem1() -> i64 {
                     contains_cid = true;
                 }
 
-                for required_field in required_fields().iter() {
+                for required_field in required_fields().keys() {
                     if field.contains(required_field) {
                         checked_fields += 1;
                         break;
@@ -52,6 +54,94 @@ pub fn problem2() -> i64 {
     0
 }
 
-fn required_fields() -> [&'static str; 8] {
-    ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
+fn required_fields() -> HashMap<&'static str, fn(&str) -> bool> {
+    let mut fields_validations: HashMap<&str, fn(&str) -> bool> = HashMap::new();
+    fields_validations.insert("byr", |val| {
+        if val.len() == 4 {
+            if let Ok(birth_year) = val.parse::<i32>() {
+                birth_year >= 1920 && birth_year <= 2002
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    });
+
+    fields_validations.insert("iyr", |val| {
+        if val.len() == 4 {
+            if let Ok(issue_year) = val.parse::<i32>() {
+                issue_year >= 2010 && issue_year <= 2020
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    });
+
+    fields_validations.insert("eyr", |val| {
+        if val.len() == 4 {
+            if let Ok(expiration_year) = val.parse::<i32>() {
+                expiration_year >= 2020 && expiration_year <= 2030
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    });
+
+    fields_validations.insert("hgt", |val| {
+        if val.contains("cm") {
+            if let Ok(cms) = val.replace("cm", "").parse::<i32>() {
+                if cms >= 150 && cms <= 193 {
+                    true
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        } else if val.contains("in") {
+            if let Ok(ins) = val.replace("in", "").parse::<i32>() {
+                if ins >= 59 && ins <= 76 {
+                    true
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    });
+
+    fields_validations.insert("hcl", |val| {
+        let re = Regex::new(r"^#(?:[0-9a-fA-F]{3}){1,2}$").unwrap();
+        return re.is_match(val);
+    });
+
+    fields_validations.insert("ecl", |val| {
+        let eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+        return eye_colors.contains(&val);
+    });
+
+    fields_validations.insert("pid", |val| {
+        if val.len() == 9 {
+            match val.parse::<i32>() {
+                Ok(_n) => true,
+                _ => false
+            }
+        } else {
+            false
+        }
+    });
+
+    fields_validations.insert("cid", |_val| true);
+
+    // println!("Resutl: {}", fields_validations.get("pid").unwrap()("0123456789"));
+
+    fields_validations
 }
