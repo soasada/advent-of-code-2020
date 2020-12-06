b58853aaf368309c5f243a5bdc01ed8bb60f7919
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-12-06 17:29:26 +0100

    Refactor day 4, doing problem 2

--- crates/day4/src/day4.rs
+++ crates/day4/src/day4.rs
@@ -3,55 +3,62 @@ use std::collections::HashMap;
 use regex::Regex;
 
 pub fn problem1() -> i64 {
-    let passports_loaded = load_str_vec_from("crates/day4/src/day4_input.txt");
     let mut valid_passports = 0;
-    let mut passport_fields = Vec::new();
+    let passport_fields = load_passport_fields();
 
-    if let Ok(passports) = passports_loaded {
-        passports.iter()
-            .map(|line| line.split(" ").collect::<Vec<&str>>())
-            .for_each(|mut vec| passport_fields.append(&mut vec));
+    let mut checked_fields = 0;
+    let mut contains_cid = false;
 
-        let mut checked_fields = 0;
-        let mut contains_cid = false;
-
-        for (i, field) in passport_fields.iter().enumerate() {
-            if *field != "" {
-                if field.contains("cid") {
-                    contains_cid = true;
-                }
-
-                for required_field in required_fields().keys() {
-                    if field.contains(required_field) {
-                        checked_fields += 1;
-                        break;
-                    }
-                }
-            } else {
-                if checked_fields >= 7 && !contains_cid {
-                    valid_passports += 1;
-                } else if checked_fields >= 8 {
-                    valid_passports += 1;
-                }
-                checked_fields = 0;
-                contains_cid = false;
+    for (i, field) in passport_fields.iter().enumerate() {
+        if *field != "" {
+            if field.contains("cid") {
+                contains_cid = true;
             }
 
-            if i == passport_fields.len() - 1 {
-                if checked_fields >= 7 && !contains_cid {
-                    valid_passports += 1;
-                } else if checked_fields >= 8 {
-                    valid_passports += 1;
+            for required_field in required_fields().keys() {
+                if field.contains(required_field) {
+                    checked_fields += 1;
+                    break;
                 }
+            }
+        } else {
+            if checked_fields >= 7 && !contains_cid {
+                valid_passports += 1;
+            } else if checked_fields >= 8 {
+                valid_passports += 1;
+            }
+            checked_fields = 0;
+            contains_cid = false;
+        }
+
+        if i == passport_fields.len() - 1 {
+            if checked_fields >= 7 && !contains_cid {
+                valid_passports += 1;
+            } else if checked_fields >= 8 {
+                valid_passports += 1;
             }
         }
     }
+
 
     valid_passports
 }
 
 pub fn problem2() -> i64 {
     0
+}
+
+fn load_passport_fields() -> Vec<String> {
+    let passports_loaded = load_str_vec_from("crates/day4/src/day4_input.txt");
+
+    if let Ok(passports) = passports_loaded {
+        return passports.iter()
+            .flat_map(|str| str.split(" "))
+            .map(str::to_owned)
+            .collect();
+    }
+
+    return Vec::new();
 }
 
 fn required_fields() -> HashMap<&'static str, fn(&str) -> bool> {
