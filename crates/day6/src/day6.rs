7041f40d3adbacddfcda6cce7e95ec67cbf0a26e
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-12-07 12:31:53 +0100

    Day 6 part 1 done

--- crates/day6/src/day6.rs
+++ crates/day6/src/day6.rs
@@ -1,7 +1,36 @@
+use files::load_str_vec_from;
+use std::collections::HashSet;
+
 pub fn problem1() -> i64 {
-    0
+    return load_declaration_forms().iter()
+        .map(|form| form.chars().collect::<HashSet<_>>())
+        .collect::<Vec<HashSet<_>>>().iter()
+        .fold(0, |acc, s| acc + s.len()) as i64;
 }
 
 pub fn problem2() -> i64 {
     0
+}
+
+fn load_declaration_forms() -> Vec<String> {
+    let declaration_forms_loaded = load_str_vec_from("crates/day6/src/day6_input.txt");
+
+    if let Ok(declaration_forms) = declaration_forms_loaded {
+        let mut group = String::new();
+        let mut groups = Vec::new();
+
+        for line in declaration_forms.iter() {
+            if line != "" {
+                group.push_str(line);
+            } else {
+                groups.push(group);
+                group = String::new();
+            }
+        }
+
+        groups.push(group);
+        return groups;
+    }
+
+    return Vec::new();
 }
