acc159419a907ea0b8e694caadeba7f525c29494
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-12-08 20:37:03 +0100

    Day 7 completed

--- crates/day7/src/day7.rs
+++ crates/day7/src/day7.rs
@@ -11,10 +11,9 @@ pub fn problem1() -> i64 {
             .map(|v| (v[0].replace(" bags", ""), parse_children_bags(v[1])))
             .collect();
 
-        let mut visited = Vec::new();
         let mut result = HashSet::new();
         for b in m.keys() {
-            visit_bags(&m, b, &mut visited, &mut result);
+            visit_bags(&m, b, &mut result);
         }
 
         return result.len() as i64;
@@ -24,6 +23,17 @@ pub fn problem1() -> i64 {
 }
 
 pub fn problem2() -> i64 {
+    let bags_restrictions_loaded = load_str_vec_from("crates/day7/src/day7_input.txt");
+
+    if let Ok(bags_restrictions) = bags_restrictions_loaded {
+        let m: HashMap<_, _> = bags_restrictions.iter()
+            .map(|line| line.split(" contain ").collect::<Vec<&str>>())
+            .map(|v| (v[0].replace(" bags", ""), parse_children_bags(v[1])))
+            .collect();
+
+        return visit_bags2(&m, "shiny gold");
+    }
+
     0
 }
 
@@ -34,19 +44,25 @@ fn parse_children_bags(children_line: &str) -> Vec<(&str, i64)> {
         .collect::<Vec<(&str, i64)>>();
 }
 
-fn visit_bags(bags: &HashMap<String, Vec<(&str, i64)>>,
-              bag_name: &String,
-              visited: &mut Vec<String>,
-              result: &mut HashSet<String>) -> bool {
+fn visit_bags(bags: &HashMap<String, Vec<(&str, i64)>>, bag_name: &String, result: &mut HashSet<String>) -> bool {
     for c in bags.get(bag_name).unwrap().iter() {
         if (*c).0 == "shiny gold" {
             result.insert(bag_name.clone());
             return true;
-        } else if visit_bags(bags, &String::from((*c).0), visited, result) {
+        } else if visit_bags(bags, &String::from((*c).0), result) {
             result.insert(bag_name.clone());
             return true;
         }
     }
 
     return false;
+}
+
+fn visit_bags2(bags: &HashMap<String, Vec<(&str, i64)>>, bag_name: &str) -> i64 {
+    let mut cont = 0;
+    for c in bags.get(bag_name).unwrap().iter() {
+        cont += (*c).1 + (*c).1 * visit_bags2(bags, (*c).0);
+    }
+
+    return cont;
 }
