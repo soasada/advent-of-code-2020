6d8f954856632a67ce25ac8ffc3ad7aaccfb5d72
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-12-10 18:52:08 +0100

    Day 10 problem 2

--- crates/day10/src/day10.rs
+++ crates/day10/src/day10.rs
@@ -27,5 +27,60 @@ pub fn problem1() -> i64 {
 }
 
 pub fn problem2() -> i64 {
+    let numbers_loaded = load_uint_vec_from("crates/day10/src/day10_input.txt");
+
+    if let Ok(mut numbers) = numbers_loaded {
+        numbers.sort();
+
+        let mut count = 0;
+        let mut one = 0;
+        let mut two = 0;
+        let mut three = 0;
+        let mut i = 0;
+
+        while i < numbers.len() - 1 {
+            let valid;
+
+            if i == 0 {
+                valid = numbers[i + 1] < 3;
+            } else {
+                valid = numbers[i + 1] - numbers[i - 1] < 3;
+            }
+
+            if count == 3 {
+                count = 0;
+                three += 1;
+            }
+
+            if valid {
+                count += 1;
+            } else {
+                if count == 2 {
+                    two += 1;
+                }
+
+                if count == 1 {
+                    one += 1;
+                }
+
+                count = 0;
+            }
+            i += 1;
+        }
+
+        if count == 3 {
+            three += 1;
+        }
+
+        if count == 2 {
+            two += 1;
+        }
+
+        if count == 1 {
+            one += 1;
+        }
+
+        return i64::pow(7, three) * i64::pow(4, two) * i64::pow(2, one);
+    }
     0
 }
