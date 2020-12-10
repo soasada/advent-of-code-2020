6a366cd6813a0947bb785a922b321f255eee5b4a
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-12-10 16:12:38 +0100

    Day 10 problem 1 solved

--- crates/day10/src/day10.rs
+++ crates/day10/src/day10.rs
@@ -1,4 +1,28 @@
+use files::load_uint_vec_from;
+
 pub fn problem1() -> i64 {
+    let numbers_loaded = load_uint_vec_from("crates/day10/src/day10_input.txt");
+
+    if let Ok(mut numbers) = numbers_loaded {
+        numbers.sort();
+        let mut i = 0;
+        let mut j = 1;
+
+        let mut jolt1 = 1;
+        let mut jolt3 = 1;
+
+        while i < numbers.len() && j < numbers.len() {
+            let difference = numbers[j] - numbers[i];
+            if difference == 1 {
+                jolt1 += 1;
+            } else if difference == 3 {
+                jolt3 += 1;
+            }
+            i += 1;
+            j += 1;
+        }
+        return jolt1 * jolt3;
+    }
     0
 }
 
